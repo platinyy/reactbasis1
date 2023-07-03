a245bbc247ca18d077298682db4f435c8f30275a
Platin Syla <platinsyla06@gmail.com>
2023-07-03 16:54:16 -0400

    Moved components into different folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
 import './App.css'
-function Header(){
-  return(
-    <header>
-    <nav className='nav'>
-        <img src="https://cdn.cdnlogo.com/logos/r/85/react.svg" className='nav-logo' />
-        <ul className='nav-items'>
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-    </nav>
-</header>
-  )
-}
-function Main(){
-  return(
-    <div className='main-content'>
-    <h1>Reasons I'm excited to learn React</h1>
-          <ol className='main-list'>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-          </ol>
-      </div>
-  )
-}
-function Footer(){
-  return (
-    <footer className='footer'>
-    <small>© 2023 Platin's development. All rights reserved.</small>
-</footer>
-  )
-}
+import Header from './components/Header'
+import Main from './components/MainContent'
+import Footer from './components/Footer'
+
+
+
 function App() {
   return (
     
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+      <footer className='footer'>
+      <small>© 2023 Platin's development. All rights reserved.</small>
+  </footer>
+    )
+  }
+  export default Footer
