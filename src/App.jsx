ed261fc1553636958f0892b746de0768a6687dff
Platin Syla <platinsyla06@gmail.com>
2023-07-03 15:20:18 -0400

    a

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+   <div>
+    <img src="https://cdn.cdnlogo.com/logos/r/85/react.svg" />
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k starts on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+   </div>
+  )
+}
+
+export default App
